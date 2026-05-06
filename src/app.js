ff13c9ae7bf745c81822b836b29b98301632d7a2
Samucodes101 <131435048+Samucodes101@users.noreply.github.com>
2026-05-06 16:22:05 +0100

    log

--- src/app.js
+++ src/app.js
@@ -89,3 +89,12 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('UNHANDLED REJECTION:', reason);
+  process.exit(1);
+});
